a9af0c98e72b0ff6755e22cca7b2badab09b429e
Alan <alan.yuan@mail.utoronto.ca>
2018-03-04 16:37:42 -0800

    Added Lambda

--- aws/Lambda/Order-Lambda/lambda.js
+++ aws/Lambda/Order-Lambda/lambda.js
@@ -1,5 +1,14 @@
 'use strict'
-const awsServerlessExpress = require('aws-serverless-express');
-const app = require('./app');
-const server = awsServerlessExpress.createServer(app);
-exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);
+const awsServerlessExpress = require('aws-serverless-express')
+const app = require('./app')
+
+// NOTE: If you get ERR_CONTENT_DECODING_FAILED in your browser, this is likely
+// due to a compressed response (e.g. gzip) which has not been handled correctly
+// by aws-serverless-express and/or API Gateway. Add the necessary MIME types to
+// binaryMimeTypes below, then redeploy (`npm run package-deploy`)
+const binaryMimeTypes = [
+  'application/json'
+]
+const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes)
+
+exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context)
