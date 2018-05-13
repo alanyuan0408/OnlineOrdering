ae0a8af54d845c6e33c5f6be89c26fe8733c2e92
Alan <alan.yuan@mail.utoronto.ca>
2018-05-13 00:36:07 -0700

    Removed Hi

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -10,7 +10,6 @@ const rootReducer = (state = initialState, action) => {
     case ADD_ARTICLE:
     	return { ...state, articles: [...state.articles, action.payload] };
    	case CLEAR_ARTICLE:
-   		console.log("hi");
    		return { ...state, articles: [] };
     default:
     	return state;
