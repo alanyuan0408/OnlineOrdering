2bf91b1ae4de0ee24f621fc8ccf9b45b9a4a64b0
Alan <alan.yuan@mail.utoronto.ca>
2018-09-09 00:24:12 -0700

    Fixed Build script

--- aws/buildscript.sh
+++ aws/buildscript.sh
@@ -2,19 +2,19 @@ echo "1) Build Script"
 
 # Generate Random Number of Builds
 min=0
-max=100
+max=10000
 rand_num=`shuf -i $min-$max -n 1`
 
 awsRegion="us-east-1"
 accountId="335783331232"
-
-CloudFormationStackName="AWS_Serverless_stack-"$rand_num
+	
+CloudFormationStackName="AWS-Serverless-stack-"$rand_num
 
 #DynamoStack Should be persistent
-DynamoStackName="dyanmoStackName"
+DynamoStackName="dynamoStackName"
 
-s3BuildBucketName="alanbuildbucket"
-s3UploadBucket="dynamoStackName-s3UploadBucket"
+s3BuildBucketName="s3buildbucket"
+s3UploadBucket="dynamostackname-s3uploadbucket"
 
 echo "2) Build Upload Bucket"
 
@@ -31,6 +31,21 @@ else
 	echo " - $s3BuildBucketName already exists, uploading build scripts"
 fi
 
+# Create S3 Upload Bucket
+if aws s3api get-bucket-location \
+	--bucket $s3UploadBucket \
+	--region $awsRegion \
+	2>&1 | grep -q 'NoSuchBucket' ; then
+
+	# S3api could not find bucket; Create new bucket
+	echo " - Making New Bucket"
+	aws s3 mb s3://$s3UploadBucket --region $awsRegion
+else
+
+	echo " - $s3UploadBucket already exists, uploading build scripts"
+fi
+
+
 echo "3) Package CloudFormation Files"
 
 # Upload CloudFormation Package to the Build Bucket
@@ -42,19 +57,43 @@ aws cloudformation package \
 
 echo "4) Deploy Data Stack "
 
-aws cloudformation deploy \
-	--template-file ./dynamotables.yaml \
+if aws cloudforamtion get-stack-policy \
 	--stack-name $DynamoStackName \
-	--capabilities CAPABILITY_IAM \
 	--region $awsRegion \
+	2>&1 | grep -q '$DynamoStackName does not exist' ; then
+
+	# Dynamo Tables Stack already exists
+	echo " - Making New Dynamo Stack"
+	aws cloudformation deploy \
+		--template-file ./dynamotables.yaml \
+		--stack-name $DynamoStackName \
+		--capabilities CAPABILITY_IAM \
+		--region $awsRegion
+else 
+
+	echo " - $s3BuildBucketName already exists, uploading build scripts"
+fi 
 
 echo "5) Deploy Application Stack"
 
-aws cloudformation deploy
+# Supports Mutiple CloudFormation Stacks
+aws cloudformation deploy \
 	--template-file packaged-sam.yaml \
 	--stack-name $CloudFormationStackName \
 	--capabilities CAPABILITY_IAM \
 	--region $awsRegion \
 	--parameter-overrides DynamoStack=$DynamoStackName
+
+echo "6) Deploy Web-build and Web-Deploys"
+
+cd ..
+
+npm run build
+
+aws s3 cp build s3://$s3UploadBucket \
+	--recursive \
+	--acl public-read-write
+
+cd ./aws
 
 echo "Build Script Complete"
